71f9c234a60d2412ac9b6f38dae222181da4f480
hmln <helio@meiralins.org>
2016-04-08 02:59:04 -0300

    adding user search endpoint

--- tapioca_instagram/resource_mapping.py
+++ tapioca_instagram/resource_mapping.py
@@ -4,5 +4,10 @@ RESOURCE_MAPPING = {
     'user': {
         'resource': 'users/{id}',
         'docs': 'https://instagram.com/developer/endpoints/users/#get_users'
-    }
+    },
+    'user_search': {
+        'resource': 'users/search',
+        'docs': 'https://instagram.com/developer/endpoints/users/#get_users_search',
+        'methods': ['GET']
+    },
 }
